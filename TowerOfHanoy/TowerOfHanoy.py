f35da0c9a29200c2a05a1cb5db3e4cf9dd2326a3
LUCAS SALIM <LSALIM@BERKELEY.EDU>
2022-03-26 15:46:26 -0700

    updating

--- TowerOfHanoy/TowerOfHanoy.py
+++ TowerOfHanoy/TowerOfHanoy.py
@@ -1,19 +1,25 @@
 from Towers import Tower
 
+# Return the list of moves needed to move n disks from start rod to end rod.
+# Each move is represented as a pair (a, b) that tells us to move top disk 
+# from rod a to rod b. 
 def f(n, start, end):
     if n == 1:
         return [(start, end)]
     return f(n-1, start, 6 - (start + end)) + f(1, start, end) + f(n-1, 6 - (start + end), end)
 
-sequence = f(8, 1, 3)
-T = Tower([8, 7, 6, 5, 4, 3, 2, 1], [], [])
-print(T)
-for pair in sequence:
-    print("Move top disk from rod " + str(pair[0]) + " to rod " + str(pair[1]) + ":")
-    myTower = [T.rod1, T.rod2, T.rod3]
-    el = myTower[pair[0] - 1].pop()
-    myTower[pair[1] - 1].append(el)
-    T = Tower(myTower[0], myTower[1], myTower[2])
+
+# Output for N disks in initial position. 
+def output(n):
+    sequence = f(n, 1, 3)
+    T = Tower([i for i in range(n, 0, -1)], [], [])
     print(T)
-    
-print("Number of moves: " + str(len(sequence)))
+    for pair in sequence:
+        print("Move top disk from rod " + str(pair[0]) + " to rod " + str(pair[1]) + ":")
+        myTower = [T.rod1, T.rod2, T.rod3]
+        el = myTower[pair[0] - 1].pop()
+        myTower[pair[1] - 1].append(el)
+        T = Tower(myTower[0], myTower[1], myTower[2])
+        print(T)
+        
+    print("Number of moves: " + str(len(sequence))) # This should always be 
